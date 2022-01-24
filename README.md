35ff1a97d7bc9f34a0887c0c9fa10ad1b0fc11b9
wisekim <lwisekiml@naver.com>
2022-01-24 22:29:42 +0900

    [스프링 DB 접근 기술] JPA

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # inflearn_spring_introduction
+
+[프로젝트 환경설정] 프로젝트 생성
+
+https://developer-khm.tistory.com/95
+
--- src/main/java/hello/hellospring/domain/Member.java
+++ src/main/java/hello/hellospring/domain/Member.java
@@ -1,8 +1,15 @@
 package hello.hellospring.domain;
 
+import javax.persistence.*;
+
+@Entity // JPA가 관리하는 엔티티
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // IDENTITY로 하면 id를 알아서 생성
     private Long id; // 시스템이 정해주는 값(sequence)
+
+//    @Column(name = "username") // 컬럼명이 username이다 라는 뜻
     private String name;
 
     public Long getId() {
